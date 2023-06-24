f79ee39199f6f603553394872f179b70413a408f
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-06-24 05:26:25 -0700

    create a file in /tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Create a file using Puppet
+file { '/tmp/school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
+}

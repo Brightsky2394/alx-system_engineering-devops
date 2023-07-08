2a34fc172dbe564bb4f455cfd177c9eedccb791a
skycode <sikiruyaya1@gmail.com>
2023-07-08 11:37:45 -0700

    takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL sends a request to that URL and displays the size of the body of the response
-curl -sI "$1" | grep 'Content-Length' | cut -d ' ' -f2
+# a script to check the size of the body of the response
+curl -i $1 | grep Content-Length | tail -c 4

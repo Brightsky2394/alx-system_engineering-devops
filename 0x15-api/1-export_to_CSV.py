5f6c7bf79d4ba4678f6378b83bd37609f36feb54
skycode <sikiruyaya1@gmail.com>
2023-07-24 11:55:08 -0700

    export data in the CSV format.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,10 +6,10 @@ import csv
 
 
 if __name__ == '__main__':
-        api_url = 'https://jsonplaceholder.typicode.com'
-        usr = get(api_url + '/users/' + argv[1]).json()['username']
-        todos = get(api_url + '/todos?userId=' + argv[1]).json()
-        with open('{}.csv'.format(argv[1]), mode='w') as file:
-            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-            for todo in todos:
-                writer.writerow([argv[1], usr, todo['completed'], todo['title']])
+    api_url = 'https://jsonplaceholder.typicode.com'
+    usr = get(api_url + '/users/' + argv[1]).json()['username']
+    todos = get(api_url + '/todos?userId=' + argv[1]).json()
+    with open('{}.csv'.format(argv[1]), mode='w') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            writer.writerow([argv[1], usr, todo['completed'], todo['title']])

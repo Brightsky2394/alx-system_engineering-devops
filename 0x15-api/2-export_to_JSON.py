41f9e6fca43d269c1215b966563b6f5f20837dae
skycode <sikiruyaya1@gmail.com>
2023-07-24 12:41:26 -0700

    export data in the JSON format.

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         record['completed'] = todo['completed']
         record['username'] = usr
         group.append(record)
-        record = dict()
+        record = {}
     user_data[argv[1]] = group
     with open('{}.json'.format(argv[1]), mode='w') as fd:
         json.dump(user_data, fd)

87d02a4639b157fe9d114b43436b5f3d38caf820
skycode <sikiruyaya1@gmail.com>
2023-08-08 12:15:26 -0700

    queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
-"""queries the Reddit API"""
+"""Queries the Reddit API and
+returns a list containing the
+titles of all hot articles for
+a given subreddit.
+"""
 from requests import get
 
 
-def recurse(subreddit, hot_list=[]):
-    """returns a list containing the titles
-    of all hot articles for a given subreddit"""
-
+def recurse(subreddit, hot_list=[], after=''):
+    """Returns a list containing the titles of all
+    hot articles for a given subreddit.
+    """
     base_url = 'https://www.reddit.com'
     api_url = '{base}/r/{sub}/hot.json'.format(base=base_url, sub=subreddit)
+
     user_agent = {'user-agent': 'my user agent 1.2'}
-    payload = {'after': after, 'limit': 100}
-    response = get(api_url, headers=user_agent, params=payload,
-                   allow_redirects=False)
-    if response.status_code == 200:
+
+    payload = {'after': after, 'limit': '100'}
+
+    response = requests.get(api_uri, headers=user_agent,
+                            params=payload, allow_redirects=False)
+    if res.status_code == 200:
         results = response.json()
         data = results.get('data')
         hot_posts = data.get('children')
+        after = data.get('after')
+
         for post in hot_posts:
-            post_data = post.get('data')
-            hot_list.append(post_data.get('title'))
+            hot_list.append(post.get('data').get('title'))
+
         if after is not None:
             recurse(subreddit, hot_list, after)
+
         return hot_list
+
     return None

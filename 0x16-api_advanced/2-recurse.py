1a5a92ff3cfb8f667f21f6be4d42fa1a00ad2c6f
skycode <sikiruyaya1@gmail.com>
2023-08-08 12:04:16 -0700

    queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""queries the Reddit API"""
+from requests import get
+
+
+def recurse(subreddit, hot_list=[]):
+    """returns a list containing the titles
+    of all hot articles for a given subreddit"""
+
+    base_url = 'https://www.reddit.com'
+    api_url = '{base}/r/{sub}/hot.json'.format(base=base_url, sub=subreddit)
+    user_agent = {'user-agent': 'my user agent 1.2'}
+    payload = {'after': after, 'limit': 100}
+    response = get(api_url, headers=user_agent, params=payload,
+                   allow_redirects=False)
+    if response.status_code == 200:
+        results = response.json()
+        data = results.get('data')
+        hot_posts = data.get('children')
+        for post in hot_posts:
+            data = post.get('data')
+            hot_list.append(data.get('title'))
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+        return hot_list
+    return None

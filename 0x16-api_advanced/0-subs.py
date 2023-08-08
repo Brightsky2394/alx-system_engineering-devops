c0b473274d5fbe24f96c14f3b96bb2f4a4fd7a76
skycode <sikiruyaya1@gmail.com>
2023-08-08 11:05:10 -0700

    queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" queries the Reddit API """
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """ returns the number of subscribers
+    (not active users, total subscribers)
+    for a given subreddit """
+    base_url = "https://www.reddit.com"
+    api_url = f"{base_url}/r/{subreddit}/about.json"
+    user_agent = {'user-agent': 'My user agent 1.2'}
+    response = get(api_url, headers=user_agent, allow_redirects=False)
+    if response.status_code in [302, 404]:
+        return 0
+    return response.json().get('data').get('subscribers')
